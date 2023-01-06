22145a29ffb48acb177fd3f46fb8437620047679
gayathri-kayapati <gayathrikayapati@gmail.com>
2023-01-06 20:22:32 +0530

    js:get the reference to the input procesing and output elements

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const button = document.querySelector("#btn");
+const noOfNotes = document.querySelector(".noOfNotes");
+var notes = [2000, 500, 100, 20, 10, 5, 1];

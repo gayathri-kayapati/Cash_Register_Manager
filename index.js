1476c60797991d5def6a6e73b84cb683a046267a
gayathri-kayapati <gayathrikayapati@gmail.com>
2023-01-06 22:32:32 +0530

    js:get the reference for error message

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const button = document.querySelector("#btn");
+const errorMessage = document.querySelector("#error-message");
 const noOfNotes = document.querySelector(".noOfNotes");
 var notes = [2000, 500, 100, 20, 10, 5, 1];

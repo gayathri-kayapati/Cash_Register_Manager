507d906a25f3915c2f5998a234be707ef37174ae
gayathri-kayapati <gayathrikayapati@gmail.com>
2023-01-07 11:27:02 +0530

    fix:bug fixing

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const billAmount = document.getElementById("bill-amount");
-const cashGiven = document.querySelector("#cash-given");
+const billAmountInput = document.querySelector("#bill-amount");
+const cashGivenInput = document.querySelector("#cash-given");
 const button = document.querySelector("#btn");
 const errorMessage = document.querySelector("#error-message");
-const noOfNotes = document.querySelector(".noOfNotes");
+const noOfNotes = document.querySelectorAll(".noOfNotes");
 var notes = [2000, 500, 100, 20, 10, 5, 1];
 
 function clickEventlistner() {
+  var billAmount = billAmountInput.value;
+  var cashGiven = cashGivenInput.value;
   if (billAmount > 0) {
     if (cashGiven >= billAmount) {
       hiddenErrorMessage();
       var returnAmount = cashGiven - billAmount;
-      for (let i = 0; i < notes.length - 1; i++) {
+      for (let i = 0; i < notes.length; i++) {
         var numberOfNotes = Math.trunc(returnAmount / notes[i]);
-        noOfNotes.innerText = numberOfNotes;
+        noOfNotes[i].innerText = numberOfNotes;
         returnAmount = returnAmount % notes[i];
       }
     } else {
@@ -24,10 +26,10 @@ function clickEventlistner() {
 }
 
 function hiddenErrorMessage() {
-  errorMessage.style.disply = "none";
+  errorMessage.style.display = "none";
 }
 
 function showErrorMessage(message) {
-  errorMessage.style.disply = "block";
+  errorMessage.style.display = "block";
   errorMessage.innerText = message;
 }

139b5e0fc3cd7eb0a331289f1c04b7f9b60bda01
gayathri-kayapati <gayathrikayapati@gmail.com>
2023-01-07 10:59:16 +0530

    js:added button click event functionality

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-const billAmount = document.querySelector("#bill-amount");
+const billAmount = document.getElementById("bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const button = document.querySelector("#btn");
 const errorMessage = document.querySelector("#error-message");
 const noOfNotes = document.querySelector(".noOfNotes");
 var notes = [2000, 500, 100, 20, 10, 5, 1];
+
+function clickEventlistner() {
+  if (billAmount > 0) {
+    if (cashGiven >= billAmount) {
+      hiddenErrorMessage();
+      var returnAmount = cashGiven - billAmount;
+      for (let i = 0; i < notes.length - 1; i++) {
+        var numberOfNotes = Math.trunc(returnAmount / notes[i]);
+        noOfNotes.innerText = numberOfNotes;
+        returnAmount = returnAmount % notes[i];
+      }
+    } else {
+      showErrorMessage("check2");
+    }
+  } else {
+    showErrorMessage("check1");
+  }
+}
+
+function hiddenErrorMessage() {
+  errorMessage.style.disply = "none";
+}
+
+function showErrorMessage(message) {
+  errorMessage.style.disply = "block";
+  errorMessage.innerText = message;
+}

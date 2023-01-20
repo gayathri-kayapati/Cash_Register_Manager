a5bc59213a49f5751a5643093accc31813b17fe2
gayathri-kayapati <gayathrikayapati@gmail.com>
2023-01-20 12:51:59 +0530

    feat:added next button

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 const billAmountInput = document.querySelector(".bill-amount");
+const nextBtn = document.querySelector("#next-btn");
+const secondaryInput = document.querySelector(".secondary-input");
 const cashGivenInput = document.querySelector(".cash-given");
 const button = document.querySelector("#btn");
 const errorMessage = document.querySelector("#error-message");
 const noOfNotesArr = document.querySelectorAll(".noOfNotes");
 var notes = [2000, 500, 100, 20, 10, 5, 1];
-var specialChar = "/^[!@#$%^&*()_+-=[]{};'\\|,.<>/?]*$/;";
+// var specialChar = "/^[!@#$%^&*()_+-=[]{};'\\|,.<>/?]*$/;";
 
-function clickEventlistner() {
+hiddenSecondaryInput();
+function nextBtnEventlistner() {
+  var billAmount = parseInt(billAmountInput.value);
+  if (billAmount > 0) {
+    showSecondaryInput();
+  } else {
+    showErrorMessage(
+      "Please enter proper bill amount, input should be a positive number."
+    );
+  }
+}
+
+function showSecondaryInput() {
+  secondaryInput.style.display = "block";
+}
+
+function checkBtnEventlistner() {
   var billAmount = parseInt(billAmountInput.value);
   var cashGiven = parseInt(cashGivenInput.value);
-  if (billAmount > 0) {
-    if (cashGiven >= billAmount) {
-      hiddenErrorMessage();
-      var returnAmount = cashGiven - billAmount;
-      for (let i = 0; i < notes.length; i++) {
-        noOfNotesArr[i].innerText = Math.trunc(returnAmount / notes[i]);
-        returnAmount = returnAmount % notes[i];
-      }
-    } else {
-      showErrorMessage("Please give more than or equal to bill amount");
+  if (cashGiven >= billAmount) {
+    hiddenErrorMessage();
+    var returnAmount = cashGiven - billAmount;
+    for (let i = 0; i < notes.length; i++) {
+      noOfNotesArr[i].innerText = Math.trunc(returnAmount / notes[i]);
+      returnAmount = returnAmount % notes[i];
     }
   } else {
-    showErrorMessage("Please enter proper bill amount");
+    showErrorMessage(
+      "Please give more than or equal to bill amount, input should be a positive number."
+    );
   }
+}
+
+function hiddenSecondaryInput() {
+  secondaryInput.style.display = "none";
 }
 
 function hiddenErrorMessage() {
